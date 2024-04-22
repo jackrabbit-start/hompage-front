19d7393ea370bc7fbafec3bf8b42410d82fbc88d
mikle8244 <mikle8244@gmail.com>
2024-04-23 02:11:41 +0900

    [setting] 폰트 적용, 기본 틀 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="bg-red-500">hello world</div>;
+  return (
+    <div className="bg-gray-300 mx-[1rem] h-full rounded-2xl text-white p-4 bg-opacity-30">
+      hello worldsfawef
+    </div>
+  );
 }
