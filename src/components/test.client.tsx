d1150a584ce39a666049ef7ae165d7200caf9e77
mikle8244 <mikle8244@gmail.com>
2024-04-23 23:10:39 +0900

    [feat] 모델 적용해보기

--- src/components/test.client.tsx
+++ src/components/test.client.tsx
@@ -1,27 +1,48 @@
 "use client";
 
 import * as THREE from "three";
-import { createRoot } from "react-dom/client";
-import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
-import { useRef, useState } from "react";
-
-import dynamic from "next/dynamic";
+import {
+  useFrame,
+  ThreeElements,
+  useThree,
+  useLoader,
+} from "@react-three/fiber";
+import { useEffect, useRef, useState } from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls, useScroll } from "@react-three/drei";
+import { Text } from "@react-three/drei";
 
 const Box = (props: ThreeElements["mesh"]) => {
   const meshRef = useRef<THREE.Mesh>(null!);
+  const gltf = useLoader(GLTFLoader, "/retro_computer_setup_free/scene.gltf");
+  const { camera } = useThree();
+  const scroll = useScroll();
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
-  useFrame((state, delta) => (meshRef.current.rotation.x += delta));
+
+  useEffect(() => {
+    camera.position.z = 20;
+    camera.position.y = 5;
+  }, [camera]);
+
+  useFrame((state, delta) => {
+    meshRef.current.rotation.y += delta / 2;
+  });
+
   return (
     <mesh
       {...props}
       ref={meshRef}
-      scale={active ? 5 : 1}
+      scale={active ? 0.2 : 0.1}
       onClick={(event) => setActive(!active)}
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}
     >
-      <boxGeometry args={[1, 1, 1]} />
+      <OrbitControls />
+
+      <primitive object={gltf.scene} />
+
+      {/* <boxGeometry args={[1, 1, 1]} />*/}
       <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
     </mesh>
   );
